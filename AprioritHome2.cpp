f45f945419376988417f1ddeb9eebf6c6fef79e1
Greshch <greshchook@gmail.com>
2022-06-16 18:06:11 +0300

    first commit

---
+++ AprioritHome2.cpp
@@ -0,0 +1,24 @@
+// AprioritHome2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//Заимплементить приложение анкеты которое должно запросить у пользователя :
+//First name
+//Last name
+//Age
+//Daily wage
+//Position
+//
+//Сохранить полученные данные и вывести в формате :
+//Name: Полное Имя;
+//Age: возраст;
+//TermUntilRetirement: количество лет до пенисии(пенсия в 55)
+//AnnualSalary(годовая зп за вычетом налогов 20 % .В году 250 рабочих дней)
+//Position : должность
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
