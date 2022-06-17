1466dfa9145253c63e4709d822aa7ade597504ec
Greshch <greshchook@gmail.com>
2022-06-17 21:00:00 +0300

    it was fixed global and pensioner

--- AprioritHome2.cpp
+++ AprioritHome2.cpp
@@ -17,42 +17,55 @@
 
 
 #include <iostream>
-#define WIN_CLS "cls"
-#define LINUX_CLS "clear"
-#define OS_CLS WIN_CLS
-
-int const RETIREMENT = 55;
-int const WORKS_DAY_IN_A_YEAR = 250;
-double const TAX = 20.0 / 100.0;
 
 
 
 int main()
 {
-    std::string firstName = "";
+    char const*  CLEAR = "cls";
+    int const RETIREMENT = 55;
+    int const WORKS_DAY_IN_A_YEAR = 250;
+    double const TAX = 20.0 / 100.0;
+
+    std::string firstName;
     std::cout << "First name: ";
     std::cin >> firstName;
-    std::string lastName = "";
+    std::string lastName;
     std::cout << "Last name: ";
     std::cin >> lastName;
     int age = 0;
     std::cout << "Age: ";
     std::cin >> age;
+
+    bool pensioner = true;
     double dailyWage = 0;
-    std::cout << "Daily wage: ";
-    std::cin >> dailyWage;
-    std::string position = "";
-    std::cout << "Position: ";
-    std::cin >> position;
-    
-    system(OS_CLS);
+    std::string position;
+    if (age < RETIREMENT)
+    {
+        pensioner = false;
+        std::cout << "Daily wage: ";
+        std::cin >> dailyWage;
+        
+        std::cout << "Position: ";
+        std::cin >> position;
+    }
+    else
+    {
+        position = "pensioner";
+    }
+    system(CLEAR);
     std::cout << "Name: " << firstName << " " << lastName << ";\n";
     std::cout << "Age: " << age << ";\n";
     
-    int termUntilRetirement = RETIREMENT - age;
-    std::cout << "TermUntilRetirement: " << termUntilRetirement << ";\n";
+    if (false == pensioner)
+    {
+        int termUntilRetirement = RETIREMENT - age;
+        std::cout << "TermUntilRetirement: " << termUntilRetirement << ";\n";
 
-    double annualSalary = WORKS_DAY_IN_A_YEAR * dailyWage * (1.0 - TAX);
-    std::cout << "AnnualSalary: " << annualSalary << ";\n";
+        double annualSalary = WORKS_DAY_IN_A_YEAR * dailyWage * (1.0 - TAX);
+        std::cout << "AnnualSalary: " << annualSalary << ";\n";
+    }
+    std::cout << "Position: " << position << ";\n";
+    
     return 0;
 }
